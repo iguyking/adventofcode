dac3dadd47fda42c6b268361ca784f8e0a0310b6
Ian K <iguyking@gmail.com>
2020-12-09 22:19:49 -0600

    day 5 updates

--- 2020/day5/day5.ps1
+++ 2020/day5/day5.ps1
@@ -1,5 +1,4 @@
-$boarding = "BBFFBBFRLL"
-
+$passes = get-content input
 
 
 function bsp {
@@ -16,49 +15,27 @@ function bsp {
             "B" { $bottom = $mid }
             "R" { $bottom = $mid }
         }
-        write-host "$step $bottom $top"
+#        write-host "$step $bottom $top"
     }
-    write-host "function $start, $end, $bottom, $top, $code, $top"
+#    write-host "function $start, $end, $bottom, $top, $code, $top"
     return $top
 }
 
-$row = bsp 0 7 0 127  $boarding
-$column = bsp 7 11 0 7  $boarding 
-$seatid = ($row*8) + $column
-write-host "Final: $row $column $seatid"
+$allpasses = New-Object System.Collections.Generic.List[System.Object]
 
-$top = 127
-$bottom = 0 
-    for($i=0; $i -lt 7; $i++) {
-    
-        $mid = [int]($top + $bottom + 1) / 2
-    
-        $step = $boarding[$i]
-        switch($step) {
-            "F" { $top = $mid - 1 }
-            "B" { $bottom = $mid }
-        }
-        write-host "$step $bottom $top"
-    }
-
-
-$row = $top
-
-$top = 7
-$bottom = 0
-
-for($j=7; $j -lt 11; $j++) {
-
-    $mid = [int]($top + $bottom + 1) / 2
-
-    $step = $boarding[$j]
-    switch($step) {
-        "L" { $top = $mid - 1 }
-        "R" { $bottom = $mid }
-    }
-    write-host "$step $bottom $top"
+foreach ($boarding in $passes) {
+    $row = bsp 0 7 0 127  $boarding
+    $column = bsp 7 11 0 7  $boarding 
+    $seatid = ($row*8) + $column
+#    write-host "Final: $boarding $row $column $seatid"
+    $allpasses.add($seatid)
 }
 
-$column = $top
-$seatid = ($row*8) + $column
-write-host "Final: $row $column $seatid"
+$measure = $allpasses | measure -maximum -minimum
+
+$allseats = $measure.minimum..$measure.maximum
+
+$myseat = $allseats | ?{$allpasses -notcontains $_}
+
+write-host "Day 5 Part 1: Highest Seat Number $($measure.maximum)"
+write-host "Day 5 Part 2: My Seat Number $myseat"
